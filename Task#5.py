e1591b389c37512dee51a4f9197d74209ebf92c4
Stanislav Volokhov <lamantina123@gmail.com>
2022-10-07 17:54:33 +0400

    add Task_5

---
+++ Task#5.py
@@ -0,0 +1,17 @@
+fibonacci = int(input('Input number: '))
+
+
+def get_fibonacci(n):
+    fibo_nums = []
+    a, b = 1, 1
+    for i in range(n-1):
+        fibo_nums.append(a)
+        a, b = b, a + b
+    a, b = 0, 1
+    for i in range (n):
+        fibo_nums.insert(0, a)
+        a, b = b, a - b
+    return fibo_nums
+
+
+print(get_fibonacci(fibonacci+1))
